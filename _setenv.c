899fa1b2087c08ae786a8f26bbf27cd08feeaf9a
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-22 09:18:40 -0500

    Update _setenv.c

--- _setenv.c
+++ _setenv.c
@@ -10,23 +10,27 @@
  */
 int _setenv(const char *varname, const char *varvalue, int overwrite)
 {
+	char *existing_value;
+	char *p;
+	size_t size;
+	int n;
 	if (varname == NULL || varname[0] == '\0' || strchr(varname, '=') != NULL)
 		return (-1);
 
-	char *existing_value = _getenv(varname);
+	existing_value = _getenv(varname);
 
 	if (existing_value != NULL && !overwrite)
 		return (-1);
 
-	size_t size = _strlen(varname) + _strlen(varvalue) + 2;
-	char *p = malloc(sizeof(char) * size);
+	size = _strlen(varname) + _strlen(varvalue) + 2;
+	p = malloc(sizeof(char) * size);
 
 	if (p == NULL)
 		return (-1);
 
 	snprintf(p, size, "%s=%s", varname, varvalue);
 
-	int n = _putenv(p);
+        n = _putenv(p);
 
 	if (n != 0)
 	{
