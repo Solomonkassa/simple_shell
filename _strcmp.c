9397ead85caa96633dda12afbaa03edb0c608fe1
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-22 01:56:15 -0500

    Update _strcmp.c

--- _strcmp.c
+++ _strcmp.c
@@ -1,9 +1,17 @@
 #include "shell.h"
 
-int _strcmp(const char* str1, const char* str2)
+/**
+ * _strcmp - compares two strings
+ * @str1: first string to compare
+ * @str2: second string to compare
+ *
+ * Return: an integer less than, equal to, or greater than zero if str1 is
+ * found, respectively, to be less than, to match, or be greater than str2.
+ */
+int _strcmp(const char *str1, const char *str2)
 {
     while (*str1 && *str2 && (*str1 == *str2))
-      {
+    {
         str1++;
         str2++;
     }
