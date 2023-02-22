92ae900f1884d7e85a295739c83a0be02af25300
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-22 09:07:02 -0500

    Create _strtok.c

---
+++ _strtok.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * _strtok - function that tokenizes a string
+ * @str: the string to tokenize
+ * @delim: the delimiter to use
+ *
+ * Return: a pointer to the next token in the string
+ */
+char *_strtok(char *str, const char *delim)
+{
+        static char *last;
+        char *token;
+
+        if (str != NULL)
+                last = str;
+
+        if (*last == '\0')
+                return NULL;
+
+        token = last;
+
+        while (*last != '\0')
+        {
+                const char *d = delim;
+
+                for (d = delim; *d != '\0'; d++)
+                {
+                        if (*last == *d)
+                        {
+                                *last = '\0';
+                                last++;
+                                return (token);
+                        }
+                }
+
+                last++;
+        }
+
+        return (token);
+}
