f66d6c167857177a9da0831fc84c9055a9befbb9
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-22 01:39:45 -0500

    Update shell.h

--- shell.h
+++ shell.h
@@ -8,7 +8,11 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+#define MAX_LINE 80
+#define MAX_ARGS 20
+
 extern char **environ;
+int last_exit_code;
 
 char* _getline(void);
 char* _strtok(char *str, const char *delim);
@@ -18,5 +22,9 @@ char *_getenv(const char *name);
 int _setenv(const char *_varname, const char *_varvalue, int overwrite);
 int _unsetenv(char *_varname);
 int _putenv(char *s);
+
+void read_command(char *line, char **args);
+void execute_command(char **args);
+
 
 #endif
