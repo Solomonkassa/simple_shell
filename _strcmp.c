e018f14d4ccc3fc8f21ac96a738533dce95efe48
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-02-22 18:45:29 +0300

    Update _strcmp.c

--- _strcmp.c
+++ _strcmp.c
@@ -10,10 +10,11 @@
  */
 int _strcmp(const char *str1, const char *str2)
 {
-    while (*str1 && *str2 && (*str1 == *str2))
-    {
-        str1++;
-        str2++;
-    }
-    return (*str1 - *str2);
+	while (*str1 && *str2 && (*str1 == *str2))
+	{
+		str1++;
+		str2++;
+	}
+
+	return (*str1 - *str2);
 }
