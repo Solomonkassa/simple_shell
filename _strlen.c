53137f678ee09656b44a8a6e9e333ca21e25d08f
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-22 09:06:16 -0500

    Create _strlen.c

---
+++ _strlen.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: string to calculate length
+ * Return: Length of the string
+ */
+
+int _strlen(const char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+
+    return (i);
+}
+
