81a6f36a3a63564f45c9645aa5829ac56dc37525
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-22 01:49:51 -0500

    Update _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -1,9 +1,10 @@
 #include "shell.h"
 
 /**
- * main - _getenv
- * 
- * Return: NULL
+ * _getenv - gets the value of the specified environment variable
+ * @name: the name of the environment variable
+ *
+ * Return: the value of the environment variable or NULL if it doesn't exist
  */
 
 char *_getenv(const char *name)
@@ -16,14 +17,10 @@ char *_getenv(const char *name)
         j = 0;
 
         while (name[j] != '\0' && name[j] == environ[i][j])
-        {
             j++;
-        }
-      
-        while (name[j] == '\0' && environ[i][j] == '=')
-        {
+
+        if (name[j] == '\0' && environ[i][j] == '=')
             return (&environ[i][j + 1]);
-        }
     }
 
     return (NULL);
