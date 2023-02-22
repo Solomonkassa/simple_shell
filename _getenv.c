2ba7f98d9d08a4c96a5b91f044cb200f771e2926
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-02-22 18:35:26 +0300

    Update _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -9,19 +9,23 @@
 
 char *_getenv(const char *name)
 {
-    int i, j;
-    extern char **environ;
+	int i, j;
+	char **environ;
 
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        j = 0;
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		j = 0;
 
-        while (name[j] != '\0' && name[j] == environ[i][j])
-            j++;
+		while (name[j] != '\0' && name[j] == environ[i][j])
+		{
+			j++;
+		}
 
-        if (name[j] == '\0' && environ[i][j] == '=')
-            return (&environ[i][j + 1]);
-    }
+		if (name[j] == '\0' && environ[i][j] == '=')
+		{
+			return (&environ[i][j + 1]);
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
